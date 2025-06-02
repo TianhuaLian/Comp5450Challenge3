b0717f3f87b7bacd4ff098217752cee928659c34
Jun Liu <jliu1296@lakeheadu.ca>
2025-06-01 23:57:36 -0400

    June's first commit
    Add start screen
    
    Signed-off-by: Jun Liu <jliu1296@lakeheadu.ca>

---
+++ lib/start_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  final VoidCallback onStart;
+
+  const StartScreen({super.key, required this.onStart});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/background.png'),
+              fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Ten-Pin Bowling',
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    letterSpacing: 2,
+                  ),
+                ),
+                SizedBox(height: 100),
+                ElevatedButton(
+                  onPressed: onStart,
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                    backgroundColor: Colors.orangeAccent,
+                  ),
+                  child: Text(
+                    'Start Game',
+                    style: TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
